f7f63b86d82dfe7fe45fb1570cdd6a47a3c80c7a
Anmol Sharma <keepupsharma@gmail.com>
2024-02-19 20:31:44 -0600

    Part 1 done

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   root "products#index"
 
+  get "/articles", to: "articles#index"
+
   resources :products
 
   get "up" => "rails/health#show", as: :rails_health_check
-  
+
 end
