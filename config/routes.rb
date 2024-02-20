bcb70f38415480b4e9d6224844c16ad08a7c6c83
Anmol Sharma <keepupsharma@gmail.com>
2024-02-19 21:14:20 -0600

    final commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "products#index"
 
-  get "/articles", to: "articles#index"
+  get "/products", to: "products#index"
 
   resources :products
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,19 +8,53 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-require 'faker';
+require "csv"
 
-676.times do
-    title  = Faker::Commerce.product_name
-    price  = Faker::Commerce.price
-    stock  = Faker::Number.number
+csv_file = Rails.root.join('db/products.csv')
+csv_data = File.read(csv_file)
 
-    newProd = Product.create(
-        title:title,
-        price:price,
-        stock_quantity:stock
+products = CSV.parse(csv_data, headers: true)
+
+products.each do |product|
+
+    # cat = Category.find(name:product.category);
+
+    # if(cat.count == 0)
+    # new_cat = Category.create(product.category)
+    # cat  = new_cat
+    # end
+
+    cat = Category.find_or_create_by(name:product['category'])
+
+    newProd = cat.products.build(
+        title:product['name'],
+        price:product['price'],
+        description:product['description'],
+        stock_quantity:product['stock quantity']
     )
 
     newProd.save()
 
+  
+
 end
+
+
+
+# Part 1
+# require 'faker';
+
+# 676.times do
+#     title  = Faker::Commerce.product_name
+#     price  = Faker::Commerce.price
+#     stock  = Faker::Number.number
+
+#     newProd = Product.create(
+#         title:title,
+#         price:price,
+#         stock_quantity:stock
+#     )
+
+#     newProd.save()
+
+# end
