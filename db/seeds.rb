e2d4fd7887f1562ba62a6c6d9a1ad4784e888c1d
Anmol Sharma <keepupsharma@gmail.com>
2024-02-19 20:26:13 -0600

    9.    Write a short db/seeds.rb file to populate the products table with 676 products. Fake the required product properties using the faker gem. For now it's only necessary to fake the title, price (using Faker::Commerce) and stock quantity (using Faker::Number).
    10.  Run your seed script and then make your next git commit. Remember that the seed script needs to be executed using: rails db:seed
    
    11.  Use the rails command line controller generator to create a Products controller with an index action/view.
    
    12.  Add your next git commit.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,20 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'faker';
+
+676.times do
+    title  = Faker::Commerce.product_name
+    price  = Faker::Commerce.price
+    stock  = Faker::Number.number
+
+    newProd = Product.create(
+        title:title,
+        price:price,
+        stock_quantity:stock
+    )
+
+    newProd.save()
+
+end
